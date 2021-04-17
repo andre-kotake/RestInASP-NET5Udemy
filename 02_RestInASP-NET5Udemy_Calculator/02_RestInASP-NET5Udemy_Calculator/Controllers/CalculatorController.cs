efdaf86e7e1f9cc7053798e0260912bf7954df40
André Kotake <andre.kotake@itixti.com.br>
2021-04-16 22:31:13 -0300

    Implemented Calculator. Add .gitignore

---
+++ 02_RestInASP-NET5Udemy_Calculator/02_RestInASP-NET5Udemy_Calculator/Controllers/CalculatorController.cs
@@ -0,0 +1,115 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _02_RestInASP_NET5Udemy_Calculator.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CalculatorController : ControllerBase
+    {
+        private readonly ILogger<CalculatorController> _logger;
+
+        public CalculatorController(ILogger<CalculatorController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("sum/{firstNumber}/{secondNumber}")]
+        public IActionResult Sum(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                decimal result = ConvertToDecimal(firstNumber) + ConvertToDecimal(secondNumber);
+                return Ok(result.ToString());
+            }
+
+            return BadRequest("Invalid Input.");
+        }
+
+        [HttpGet("subtract/{firstNumber}/{secondNumber}")]
+        public IActionResult Subtract(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                decimal result = ConvertToDecimal(firstNumber) - ConvertToDecimal(secondNumber);
+                return Ok(result.ToString());
+            }
+
+            return BadRequest("Invalid Input.");
+        }
+
+        [HttpGet("multiply/{firstNumber}/{secondNumber}")]
+        public IActionResult Multiply(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                decimal result = ConvertToDecimal(firstNumber) * ConvertToDecimal(secondNumber);
+                return Ok(result.ToString());
+            }
+
+            return BadRequest("Invalid Input.");
+        }
+
+        [HttpGet("divide/{firstNumber}/{secondNumber}")]
+        public IActionResult Divide(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                decimal secondNumberDecimal = ConvertToDecimal(secondNumber);
+
+                if (secondNumberDecimal > 0)
+                {
+                    decimal result = ConvertToDecimal(firstNumber) / secondNumberDecimal;
+                    return Ok(result.ToString());
+                }
+            }
+
+            return BadRequest("Invalid Input.");
+        }
+
+        [HttpGet("average/{firstNumber}/{secondNumber}")]
+        public IActionResult Average(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                decimal result = (ConvertToDecimal(firstNumber) + ConvertToDecimal(secondNumber)) / 2;
+                return Ok(result.ToString());
+            }
+
+            return BadRequest("Invalid Input.");
+        }
+
+        [HttpGet("square-root/{firstNumber}")]
+
+        public IActionResult SquareRoot(string firstNumber)
+        {
+            if (IsNumeric(firstNumber))
+            {
+                double result = Math.Sqrt((double)ConvertToDecimal(firstNumber));
+                return Ok(result.ToString());
+            }
+
+            return BadRequest("Invalid Input.");
+        }
+
+        private bool IsNumeric(string number)
+        {
+            return decimal.TryParse(number,
+                                    System.Globalization.NumberStyles.Any,
+                                    System.Globalization.NumberFormatInfo.InvariantInfo,
+                                    out decimal _);
+        }
+
+        private decimal ConvertToDecimal(string number)
+        {
+            if (decimal.TryParse(number, out decimal result))
+                return result;
+
+            return 0;
+        }
+    }
+}
